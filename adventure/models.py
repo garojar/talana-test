7bcf72f81c715c6a96abd58eabe8a23fc45a44af
German Rojo <grojo@techk.cl>
2021-09-10 18:00:59 -0300

    Inital commit

---
+++ adventure/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+import numpy as np
+# Create your models here.
+
+
+class VehicleType(models.Model):
+    name = models.CharField(max_length=32)
+    max_capacity = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=32)
+    passengers = models.PositiveIntegerField()
+    vehicle_type = models.ForeignKey(VehicleType, null=True, on_delete=models.SET_NULL)
+    number_plate = models.CharField(max_length=10)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def can_start(self) -> bool:
+        return self.vehicle_type.max_capacity >= self.passengers
+
+    def get_distribution(self) -> []:
+        seat_distribution = [
+            [True,True]
+            for _ in range(int(self.vehicle_type.max_capacity/2))
+        ]
+        if self.vehicle_type.max_capacity % 2 == 1:
+            seat_distribution.append([True,False])
+
+        return seat_distribution
+
+class Journey(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.PROTECT)
+    start = models.DateField()
+    end = models.DateField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.vehicle.name} ({self.start} - {self.end})"
